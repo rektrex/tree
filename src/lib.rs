8b245d120e3066a5b66a5b6593bd746dce826c81
rektrex <psaimanideep@gmail.com>
2019-10-10 18:05:53 +0530

    print number of directories and files

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ use std::fs;
 use std::path::Path;
 
 pub fn print(args: Vec<String>) -> std::io::Result<()> {
+    let mut dir_count = 0;
+    let mut file_count = 0;
+
     let mut dir = Path::new(".");
 
     if let Some(_dir) = args.get(1) {
@@ -14,11 +17,19 @@ pub fn print(args: Vec<String>) -> std::io::Result<()> {
 
     let files = fs::read_dir(dir)?;
     for file in files {
-        let file = file?.file_name();
-        if let Ok(file_name) = file.into_string() {
+        let file = file?;
+        let file_name = file.file_name();
+        if let Ok(file_name) = file_name.into_string() {
+            if fs::metadata(file.path())?.file_type().is_dir() {
+                dir_count += 1;
+            } else {
+                file_count += 1;
+            }
             println!("{}", file_name);
         }
     }
+
+    println!("\n{} directories, {} files", dir_count, file_count);
 
     Ok(())
 }
