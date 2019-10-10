6ff58bce6ff7c8b98753005983dad03162ec6b25
rektrex <psaimanideep@gmail.com>
2019-10-10 10:08:50 +0530

    use ? for if let wherever possible

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::Path;
 
-pub fn print(args: Vec<String>) {
+pub fn print(args: Vec<String>) -> std::io::Result<()> {
     let mut dir = Path::new(".");
 
     if let Some(_dir) = args.get(1) {
@@ -12,13 +12,13 @@ pub fn print(args: Vec<String>) {
         println!("{}", dir_string);
     }
 
-    if let Ok(files) = fs::read_dir(dir) {
-        for file in files {
-            if let Ok(file) = file {
-                if let Ok(file_name) = file.file_name().into_string() {
-                    println!("{}", file_name);
-                }
-            }
+    let files = fs::read_dir(dir)?;
+    for file in files {
+        let file = file?.file_name();
+        if let Ok(file_name) = file.into_string() {
+            println!("{}", file_name);
         }
     }
+
+    Ok(())
 }
